bd9d896f86a0d55afb0f3bc5ddf04773c204a0dd
Javier Simon <simonjavier.fullstack@gmail.com>
2023-11-03 10:33:32 -0300

    chore: se han agregado archivos nuevos

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+DEPLOY de nuestra pagian -> vamos a compartir nuestro proyecto al mundo
+  *-> deploy:
+    proceso de lanzar y poner en funcionamiento una app o sistema en un entorno de producción, para que este disponible y accesible para los usuarios finales
+
+  *-> objetivos:
+    - hacer el deploy -> RENDER
+    - varibales de entorno
+    - readme
+    - refactorizar
+
+? Deploy
+  - complejo y desafiante
+  - importante => el desarrollador debe entregar su trabajo final
+  - ayuda al armado del PORTFOLIO => coleccione de trabajo o proeyecto de un a persona
+  - una vez deployados cualquier persona que quiero verlo podra (futuro empleador)
+
+  * Github => es un sitio donde solemos consultar los trabajos que a realizado un desarrollador
+    - recordatorio de comandos:
+      git init
+      git add .
+      git add nombreDelArchivos.js
+      git status
+      git push -u origin master
+
+
+*/
